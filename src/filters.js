b207e099ae23bf235720f00a7565a9e8dd49fdb8
iver56 <iver56@hotmail.com>
2017-10-29 22:04:52 +0100

    Format time

---
+++ src/filters.js
@@ -0,0 +1,10 @@
+Vue.filter('formatDuration', function (numSeconds) {
+  numSeconds = Math.floor(numSeconds);
+  let minutes = Math.floor(numSeconds / 60);
+  let seconds = numSeconds - minutes * 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return `${minutes}:${seconds}`;
+});
